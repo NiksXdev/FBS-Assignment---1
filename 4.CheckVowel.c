006b14aeadb0ad7bcd0a40ecfab44f1763e0e76f
Nikhil Kangare <nikhilkangare1@gmail.com>
2026-01-14 16:24:20 +0530

    Change character input to fixed value 'a'
    Removed user input for character check and set 'ch' to 'a'.

--- 4.CheckVowel.c
+++ 4.CheckVowel.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
 int main() {
-    char ch;
-    printf("Enter a character to check :");
-    scanf("%c",&ch);
+    char ch=a;
     if((ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U') || ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
     printf("Vowels");
     else

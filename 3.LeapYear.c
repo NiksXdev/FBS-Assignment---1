b0ef1ddd1f36051514ecfc53a6714a698e81addd
Nikhil Kangare <nikhilkangare1@gmail.com>
2026-01-14 16:23:46 +0530

    Set fixed year to 2004 for leap year check
    Changed input year from user to a fixed value of 2004.

--- 3.LeapYear.c
+++ 3.LeapYear.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
 int main() {
-    int Year;
-    printf("Enter a year to check :");
-    scanf("%d",&Year);
+    int Year=2004;
     if(Year>0) {
         if(Year%4==0 && Year%400==0)
         printf("Leap Year");

867edb5f0db568cf9c338b8c8437605b03401dfa
Nikhil Kangare <nikhilkangare1@gmail.com>
2026-01-14 16:22:23 +0530

    Set fixed number to check for even or odd

--- 1.EvenOrOddNumber.c
+++ 1.EvenOrOddNumber.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
 int main() {
-    int num;
-    printf("Enter a number to check weather its Even or Odd :");
-    scanf("%d",&num);
+    int num=6547;
     if(num>0) {
         if(num%2==0)
         printf("Even");
